c8cc7e8d4f858aeaeefd5ca5ca9cedf9bceab5a0
ThePrimeagen <the.primeagen@aol.com>
2024-10-19 16:43:09 +0100

    Adding player movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -13,15 +15,33 @@ def main():
     
     # main game loop
     game_running = True
+    game_clock = pygame.time.Clock()
+    delta_time = 0
+ 
+    # initialise the player
+    player_x_pos = SCREEN_WIDTH/2
+    player_y_pos = SCREEN_HEIGHT/2
+    player = Player(player_x_pos, player_y_pos)
+    
+    # run the game
     while game_running:
-        for event in pygame.event.get()
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # update the delta time:
+        delta_time = game_clock.tick(60)/1000
+        
+        # update objects
+        player.update(delta_time)
+                
+        # render objects
         black = pygame.Color((0,0,0))
         view = screen.fill(color=black)
+        player.draw(screen)
+        
+        # flip the buffers
         pygame.display.flip()
-    
     
 if __name__ == "__main__":
     main()
